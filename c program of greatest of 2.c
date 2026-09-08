09cdfb8e14798095112a1a7f4a456c18e572227b
sudeepthireddykonegari-arch <sudeepthireddykonegari@gmail.com>
2026-09-08 11:23:54 +0530

    Create c program of greatest of 2.c

---
+++ c program of greatest of 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int x=0;
+    int y=0;
+    printf("enter the value of x\n");
+    scanf("%d",&x);
+    printf("enter the value of y\n");
+    scanf("%d",&y);
+    if(x>y){
+        printf("%d is greater",x);
+    }
+    else{
+        printf("&d is greater",y);
+    }
+ return 0;
+}
